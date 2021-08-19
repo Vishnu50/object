75cbcf33aeb4a66aecbe84383c3d1a38c586b7b1
vishnu <vishnu.v>
2021-08-19 16:35:34 +0530

    commit changes

---
+++ src/src/src/module-info.java
@@ -0,0 +1,12 @@
+public class CreateObjectExample1   
+{    
+void show()    
+{    
+System.out.println("this is vishnu");    
+}    
+public static void main(String[] args)   
+{      
+CreateObjectExample1 obj = new CreateObjectExample1();   
+ obj.show();    
+}    
+}   
